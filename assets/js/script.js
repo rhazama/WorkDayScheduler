8cc4fef51bd8fcf73a4968f6f6892178d69fa484
rhazama <rhazama01@gmail.com>
2022-03-23 17:51:11 -0700

    added the getTime function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,50 @@
-var todaysDate = moment().format('dddd MM, YYYY');
-$("#currentDay").html(todaysDate);
+var currentDate = moment().format('dddd MM, YYYY');
+$("#currentDate").html(currentDate);
 
 $(document).ready(function () {
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-    localStorage.setItem(time, text);
+        localStorage.setItem(time, text);
+    })
+
+    function getTime() {
+
+        var currentTime = moment().hour();
+
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (timeBlock < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (timeBlock === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+
+    //gets the items from local storage
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    getTime();
 })
-
-//gets the items from local storage
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
